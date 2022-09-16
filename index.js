30784ce71fd5bd6a478676b775fa0512b9a49199
Chioma-ux <okaforrchioma@gmail.com>
2022-09-16 03:30:08 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//function. for you to know how to write a function, first of all know the little formular. here we go
+//function name, parenthesis, curly open bracket, the body of the function, close curly braces. then call your function. lets put it into practice.
+
+
+//function 
+function sum (a,b) {
+ return a+b*4;
+}
+
+console.log(sum(2,3));
